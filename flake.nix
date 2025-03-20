ab3cc993c2609026783e9346830b4d5d2f718b1a
Nicola Squartini <tensor5@gmail.com>
2025-03-20 17:30:43 +0100

    chore: add `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "padigitale2026-opendata";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    flake-utils,
+    nixpkgs,
+    self,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (pkgs.python3.withPackages (python-pkgs: [
+              python-pkgs.numpy
+              python-pkgs.pandas
+              python-pkgs.requests
+            ]))
+          ];
+        };
+        formatter = pkgs.alejandra;
+      }
+    );
+}
